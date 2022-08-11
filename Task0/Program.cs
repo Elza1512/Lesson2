a04e6ddfd8e39531664d51aeedbf2352cd8888ea
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-11 19:29:47 +0400

    Тернарный оператор

--- Task0/Program.cs
+++ Task0/Program.cs
@@ -1,2 +1,25 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// 9. Напишите программу, которая
+// 1.выводит случайное число из отрезка [10, 99] и 
+// 2. показывает наибольшую цифру числа.
+// 78 -> 8
+// 12 -> 2
+// 85 -> 8
+
+int number = new Random().Next(10, 100);
+
+//Console.WriteLine($"Случайное число из диапозона 10 - 99 => {number}");
+
+int firstDigit = number / 10;
+int secondDigit = number % 10;
+
+// if (firstDigit == secondDigit) Console.WriteLine("Цифры равны");
+// else if (firstDigit > secondDigit) Console.WriteLine($"Наибольшая цифра числа {number} => {firstDigit} ");
+// else Console.WriteLine($"Наибольшая цифра числа {number} => {secondDigit} ");
+//? тогда, : иначе. СОкращенная версия кода выше. Тернарный оператор
+//int result = firstDigit > secondDigit ? firstDigit : secondDigit; 
+//Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
+
+
+ string result = firstDigit == secondDigit ? "Цифры равны" 
+ : firstDigit > secondDigit ? firstDigit.ToString() : secondDigit.ToString();
+Console.WriteLine($"Наибольшая цифра числа {number} => {result}");
