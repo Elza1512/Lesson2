85287941f8a2128ed37817be35c41b7fcb3a3e3b
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-15 16:31:29 +0400

    DZ-Z15

--- DZ_Z15/Program.cs
+++ DZ_Z15/Program.cs
@@ -1,1 +1,19 @@
-﻿
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, 
+// и проверяет, является ли этот день выходным.
+
+// 6 -> да
+// 7 -> да
+// 1 -> нет
+
+Console.WriteLine("Введите целое число: ");
+
+int a = Convert.ToInt32(System.Console.ReadLine());
+
+if( a == 1) System.Console.WriteLine("Не является выходным днем");
+else if( a == 2) System.Console.WriteLine("НеНе является выходным днем");
+else if( a == 3) System.Console.WriteLine("Не является выходным днем");
+else if( a == 4) System.Console.WriteLine("Не является выходным днем");
+else if( a == 5) System.Console.WriteLine("Не является выходным днем");
+else if( a == 6) System.Console.WriteLine("Является выходным днем");
+else if( a == 7) System.Console.WriteLine("Является выходным днем");
+else System.Console.WriteLine("Число не в диапозоне 1-7");
