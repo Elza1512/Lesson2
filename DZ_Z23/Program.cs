b008e5f40ed00ba0f860d459152cd048d67666ab
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-18 17:00:46 +0400

    DZ-Z23

--- DZ_Z23/Program.cs
+++ DZ_Z23/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу кубов чисел от 1 до N.
+
+// 3 -> 1, 8, 27
+// 5 -> 1, 8, 27, 64, 125
+
+Console.WriteLine("Введите значение: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int i = 1;
+if (num > 0)
+{
+   while (i <= num)
+   {
+       int q = i * i * i;
+       Console.WriteLine($"{i}   {q}");
+       i++;
+   }
+}
+else 
+{
+    Console.WriteLine("Введите значение больше 0!");
+}
