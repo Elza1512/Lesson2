01d3b14b9ef2f1ac115bdaec09b0119343630828
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-18 16:46:24 +0400

    DZ-Z21

--- DZ_Z21/Program.cs
+++ DZ_Z21/Program.cs
@@ -1,2 +1,29 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+// A (3,6,8); B (2,1,-7), -> 15.84
+
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+
+
+Console.Write("Введите координату x для первой точки: ");
+int x1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату y для первой точки: ");
+int y1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату z для первой точки: ");
+int z1 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату x для второй точки: ");
+int x2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату y для второй точки: ");
+int y2 = int.Parse(Console.ReadLine());
+
+Console.Write("Введите координату z для второй точки: ");
+int z2 = int.Parse(Console.ReadLine());  
+
+double result = Math.Sqrt((x2 - x1)*(x2 - x1)+(y1 - y2)*(y1 - y2) + (z2 -z1)*(z2-z1));
+Console.WriteLine("Расстояние между точками равно {0: #.##}", result);
+
