d34b4635af37f097d994852cba06eb7905473bcd
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-22 22:42:52 +0400

    42

---
+++ Tast42/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать
+// десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+Console.WriteLine("Введите целое число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int result = 0;
+int i = 1;
+while (number > 0 )
+{
+    result += number % 2 * i;
+    number = number / 2;
+    i *= 10;
+}
+
+Console.WriteLine(result);
