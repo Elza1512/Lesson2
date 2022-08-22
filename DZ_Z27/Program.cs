6919401cf16778d8d016730de4322ccb62ac4b43
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-22 19:09:06 +0400

    DZ-27

--- DZ_Z27/Program.cs
+++ DZ_Z27/Program.cs
@@ -5,3 +5,27 @@
 // 82 -> 10
 
 // 9012 -> 12
+
+Console.WriteLine("Введите число: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+
+int Sum(int num1)
+  {
+    int a = Convert.ToString(num1).Length;
+    int b = 0;
+    int result = 0;
+
+    for (int i = 0; i < a; i++)
+    {
+      b = num1 - num1 % 10;
+      result = result + (num1 - b);
+      num1 = num1 / 10;
+    }
+   return result;
+  }
+
+int sum = Sum(num1);
+Console.WriteLine("Сумма цифр в числе: " + sum);
+
+
+
