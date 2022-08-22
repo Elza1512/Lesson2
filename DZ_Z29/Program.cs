f65c73c89629e0d7a02255d92fa66396d2c2ed80
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-22 20:14:48 +0400

    DZ-Z29

--- DZ_Z29/Program.cs
+++ DZ_Z29/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+
+// 1, 2, 5, 7, 19, 6, 1, 33 -> [1, 2, 5, 7, 19, 6, 1, 33]
+
+
+int [] NewArray()
+{
+ int [] array = new int[8];
+ for (int i = 0; i <= 7; i++) 
+ {
+    array[i] = new Random().Next(0,7);
+ } 
+ return array;
+}
+int [] array = NewArray();
+void PrintArray()
+{
+    for (int i = 0; i<= 7; i++)
+    {
+        Console.Write($" {array [i]}, ");
+    }
+}
+PrintArray();
