ea1ffce250fa57dbaced68ada684dab821b5e7d1
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-22 22:37:39 +0400

    40

--- Tast40/Program.cs
+++ Tast40/Program.cs
@@ -4,32 +4,32 @@
 // Теорема о неравенстве треугольника: каждая сторона треугольника
 // меньше суммы двух других сторон
 
-Console.WriteLine("Введите число №1: ");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число №2: ");
-int num2 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число №3: ");
-int num3 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите сторону A: ");
+// int num1 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите сторону В: ");
+// int num2 = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Введите сторону С: ");
+// int num3 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num2 + num1 )
-{
-    Console.WriteLine("Является треугольником");
-}
-else Console.WriteLine("Не является треугольником");
+// if (num1 < num2 + num3 && num2 < num1 + num3 && num3 < num2 + num1 )
+// {
+//     Console.WriteLine("Является треугольником");
+// }
+// else Console.WriteLine("Не является треугольником");
 
 // Через метод
 
-// Console.Write("Введите число №1: ");
-// int num1 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число №2: ");
-// int num2 = Convert.ToInt32(Console.ReadLine());
-// Console.Write("Введите число №3: ");
-// int num3 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону A: ");
+int num1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону В: ");
+int num2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите сторону С: ");
+int num3 = Convert.ToInt32(Console.ReadLine());
 
-// bool Triangle(int a, int b, int c)
-// {
-//     return a < b + c && b < a + c && c < b + a;
-// }
-// bool result = Triangle(num1, num2, num3);
-// Console.WriteLine(result?"Является треугольником":"Не является треугольником");
+bool Triangle(int a, int b, int c)
+{
+    return a < b + c && b < a + c && c < b + a;
+}
+bool result = Triangle(num1, num2, num3);
+Console.WriteLine(result?"Является треугольником":"Не является треугольником");
 
