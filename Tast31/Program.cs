6c59f37ab7575b1b9288a27bed30bd3286d2a8fb
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-25 15:20:03 +0400

    31

---
+++ Tast31/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 31. Задайте массив из 12 элементов, заполненный случайными числами из промежутка [ -9. 9].
+// Найдите сумму отрицательных и положительных элементов массива. Вывод результата.
+
+
+int [] CreateArrayRndInt(int size, int min, int max)
+{
+    int [] array = new int [size];
+    Random rnd = new Random();
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = rnd.Next(min, max + 1);
+    }
+    return array;
+}
