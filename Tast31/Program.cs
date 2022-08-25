9d18616f531b8ca612460f18fceddf64f39cd8f6
Elza1512 <109744471+Elza1512@users.noreply.github.com>
2022-08-25 18:10:14 +0400

    31

--- Tast31/Program.cs
+++ Tast31/Program.cs
@@ -2,9 +2,9 @@
 // Найдите сумму отрицательных и положительных элементов массива. Вывод результата.
 
 
-int [] CreateArrayRndInt(int size, int min, int max)
+int[] CreateArrayRndInt(int size, int min, int max)
 {
-    int [] array = new int [size];
+    int[] array = new int[size];
     Random rnd = new Random();
     for (int i = 0; i < size; i++)
     {
@@ -12,3 +12,35 @@ int [] CreateArrayRndInt(int size, int min, int max)
     }
     return array;
 }
+
+void PrintArray(int[] array)
+{
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+       if (i < array.Length - 1) Console.Write($"{array[i]}, ");
+       else Console.Write(array[i]);
+    }
+     Console.Write("]");
+     Console.WriteLine();
+}
+
+int [] GetSumPosNegElem (int [] array) 
+{
+    int sumNeg = 0;
+    int sumPos = 0;
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array [i] < 0) sumNeg += array[i];
+        else sumPos += array[i];
+    }
+
+    return new int []{sumNeg, sumPos};
+}
+
+int [] arr = CreateArrayRndInt(12, -9, 9);
+PrintArray(arr);
+int[] sumPosNegElem = GetSumPosNegElem (arr);
+Console.WriteLine($"Сумма отрицательных чисел = {sumPosNegElem [0]}");
+Console.WriteLine($"Сумма положительных чисел = {sumPosNegElem [1]}");
